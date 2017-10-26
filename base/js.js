b064e2c69b9a8c0f6af1900823c3fa29ccdb7527
jiumi <1123051889@qq.com>
2017-10-26 13:13:45 +0800

    coding仓库转移

---
+++ base/js.js
@@ -0,0 +1,41 @@
+
+function init(url){
+	ajax({  
+    method: 'GET',  
+    url: url,  
+    success: function (response) {  
+    		console.log(response);
+       	let table = new Table(response); 
+    }  
+});
+}
+function Table(data){ 
+	this.data = data;
+	this.init();
+}
+Table.prototype.init = function(){
+	console.log(this.data); 
+}
+var url = 'base/base.json';
+init(url)
+
+//var flag;
+//function a (url){
+//	var temp;
+//	ajax({
+//		method: 'GET',  
+//  		url: url,  
+//  		async:false,
+//  		success: function (data) {  
+//     		temp = data;
+//     		flag = data;
+//     		console.log(temp);
+//     		console.log(flag)
+//  		}
+//	})	
+//	console.log(temp)
+//	console.log(flag)
+//}
+//console.log(temp);
+//console.log(flag);
+//a(url)
---
+++ js.js
@@ -0,0 +1,64 @@
+
+const TableContent= document.getElementsByClassName("table-content");
+const number = document.getElementsByClassName("number");
+
+
+function table(){
+	this.dom = dom.querySelector("table");	
+}
+table.prototype.init = function(url){
+	this.rows = this.dom.rows;
+	this.cells = this.dom.rows[0].cells;
+	if(this.dom.querySelectorAll(".number")){
+		this.numbercellIndex = this.dom.querySelector(".number").cellIndex;
+	}
+	if(this.dom.querySelectorAll(".sex")){
+		this.sexcellIndex = this.dom.querySelector(".sex").cellIndex;
+	}
+	if(this.dom.querySelectorAll(".date")){
+		this.datecellIndex = this.dom.querySelector(".date").cellIndex;
+	}
+	if(this.dom.querySelectorAll(".operation")){
+		this.operationcellIndex = this.dom.querySelector(".operation").cellIndex
+	}
+}
+
+table.prototype.update = function(s){
+	this.dom.addEventListener('click',function(e){
+		console.log(e.target)
+		var classval = e.target.className;
+		var classname = classval.split(" ")
+		if(classname[1] == 'icon-editor'){
+			var str = e.target.parentNode.innerText;
+			e.target.parentNode.firstChild.innerText = "";
+			var newdate = document.createElement("input");
+			newdate.value = str;
+			e.target.parentNode.firstChild.appendChild(newdate);
+			console.log(classval)
+			classval = classval.replace("editor","xuanzhong");
+			console.log(classval)
+			e.target.setAttribute("class",classval)
+		}
+		if(classname[1] == 'icon-xuanzhong'){
+			var child = e.target.parentNode.childNodes;
+			console.log(child)
+			var str = child[0].firstChild.value ;
+			child[0].removeChild(child[0].firstChild);
+			child[0].innerText = str
+			classval = classval.replace("xuanzhong","editor");
+			e.target.setAttribute("class",classval)
+		}
+		if(e.target.className == "delete"){
+			console.log("yes")
+			e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode)
+		}
+	})
+}
+ 
+ ajax({
+    method: 'GET',  
+    url: 'base/base.json',  
+    success: function (response) {  
+         date  = response;
+    }  
+});
