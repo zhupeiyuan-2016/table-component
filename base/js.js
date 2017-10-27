c1bc77e86d93f07eff846e2bbc794180279a8617
jiumi <1123051889@qq.com>
2017-10-27 21:47:33 +0800

    配置文件构建，解析

--- base/js.js
+++ base/js.js
@@ -2,40 +2,29 @@
 function init(url){
 	ajax({  
     method: 'GET',  
-    url: url,  
-    success: function (response) {  
-    		console.log(response);
+    url: url,
+    success: function (response) { 
        	let table = new Table(response); 
     }  
 });
 }
 function Table(data){ 
-	this.data = data;
+	this.data = JSON.parse(data);
 	this.init();
 }
 Table.prototype.init = function(){
-	console.log(this.data); 
+	const toolbar = document.getElementById(this.data.show.toolbar);
+	var table = document.createElement('table');
+	table.innerHTML = "<tr></tr>"
+	toolbar.appendChild(table);
+	var tr = toolbar.querySelector("tr");
+	for (var i = 0 ; i < this.data.columns.length-1 ; i++ ){
+		var th = document.createElement("th");
+		th.innerHTML = this.data.columns[i].title;
+		th.setAttribute("class",this.data.columns[i].class);
+		tr.appendChild(th)
+		console.log(this.data.columns[i].class)
+	}
+	
 }
-var url = 'base/base.json';
-init(url)
 
-//var flag;
-//function a (url){
-//	var temp;
-//	ajax({
-//		method: 'GET',  
-//  		url: url,  
-//  		async:false,
-//  		success: function (data) {  
-//     		temp = data;
-//     		flag = data;
-//     		console.log(temp);
-//     		console.log(flag)
-//  		}
-//	})	
-//	console.log(temp)
-//	console.log(flag)
-//}
-//console.log(temp);
-//console.log(flag);
-//a(url)
